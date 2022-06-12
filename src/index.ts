18475e4d7adb19717325abc3844bf682a3baae06
leegeunhyeok <dev.ghlee@gmail.com>
2022-06-12 17:49:31 +0900

    feat: add status view
    - update heading typo (add emoji)
    - add status text
      - service worker registration
      - push support
      - notification permission
      - subscription data preview

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 type Store = {
   pushSupport: boolean;
   serviceWorkerRegistration: ServiceWorkerRegistration | null;
+  pushSubscription: PushSubscription | null;
 }
 
 type Elements = {
+  notificationPermission: HTMLParagraphElement | null;
+  pushSupport: HTMLParagraphElement | null;
+  registration: HTMLParagraphElement | null;
+  subscription: HTMLPreElement | null;
   message: HTMLInputElement | null;
   targetUserId: HTMLInputElement | null;
 }
 
 const userId = localStorage.getItem('userId');
 const elements: Elements = {
+  // Status text
+  registration: null,
+  pushSupport: null,
+  notificationPermission: null,
+  subscription: null,
+  // Inputs
   message: null,
   targetUserId: null,
 };
@@ -17,6 +28,7 @@ const elements: Elements = {
 const store: Store = {
   pushSupport: false,
   serviceWorkerRegistration: null,
+  pushSubscription: null,
 };
 
 if (!userId) location.href = '/login.html';
@@ -31,20 +43,28 @@ async function registerServiceWorker () {
 
   store.serviceWorkerRegistration = registration ?? null;
   store.pushSupport = !!registration?.pushManager;
+  store.pushSubscription = await registration?.pushManager?.getSubscription();
+
+  updateStatus();
 }
 
 async function sendSubscriptionToServer (subscription?: PushSubscription) {
   console.log('sendSubscriptionToServer', { subscription });
 
-  if (!subscription) return;
+  if (!subscription) {
+    console.warn('sendSubscriptionToServer - subscription cannot be empty');
+    return;
+  }
 
-  await fetch('/subscription', {
+  const response = await fetch('/subscription', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ userId, subscription }),
   });
+
+  console.log('sendSubscriptionToServer', { response });
 }
 
 async function subscribeToPush () {
@@ -53,11 +73,20 @@ async function subscribeToPush () {
   console.log('subscribeToPush', { vapidPublicKey });
 
   const registration = store.serviceWorkerRegistration;
+
+  if (!registration) {
+    console.warn('subscribeToPush - service worker is not registered');
+    return;
+  }
+
   const subscription = await registration?.pushManager.subscribe({
     applicationServerKey: vapidPublicKey,
     userVisibleOnly: true,
   });
+  store.pushSubscription = subscription;
   await sendSubscriptionToServer(subscription);
+
+  updateStatus();
 }
 
 async function sendPushNotification () {
@@ -70,13 +99,29 @@ async function sendPushNotification () {
     return;
   }
 
-  await fetch('/send-push-notification', {
+  const response = await fetch('/send-push-notification', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ targetId, message }),
   });
+
+  console.log('sendPushNotification', { response });
+}
+
+async function updateStatus () {
+  const setText = (element: HTMLElement | null, value: string | boolean) => {
+    if (element) {
+      element.textContent = value.toString();
+      typeof value === 'boolean' && element.classList.add(value ? 't' : 'f');
+    }
+  }
+
+  setText(elements.registration, !!store.serviceWorkerRegistration);
+  setText(elements.pushSupport, store.pushSupport);
+  setText(elements.notificationPermission, Notification.permission);
+  setText(elements.subscription, JSON.stringify(store.pushSubscription, null, 2));
 }
 
 function logout () {
@@ -85,8 +130,13 @@ function logout () {
 }
 
 window.onload = () => {
+  elements.registration = document.getElementById('registration_status') as HTMLParagraphElement;
+  elements.pushSupport = document.getElementById('push_support_status') as HTMLParagraphElement;
+  elements.notificationPermission = document.getElementById('notification_permission_status') as HTMLParagraphElement;
+  elements.subscription = document.getElementById('subscription') as HTMLPreElement;
   elements.message = document.getElementById('message') as HTMLInputElement;
   elements.targetUserId = document.getElementById('target_user_id') as HTMLInputElement;
+  updateStatus();
 }
 
 registerServiceWorker();
